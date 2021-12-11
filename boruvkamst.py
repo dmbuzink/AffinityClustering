fbf244434a3fc6f57bc0d7e74dfd343a6bb848b4
Damian M. Buzink <dbuzink@gmail.com>
2021-12-11 23:10:24 +0100

    Added PySpark esque stuff (still needs testing it actually works)

--- boruvkamst.py
+++ boruvkamst.py
@@ -3,32 +3,46 @@ from helper_classes import Vertex, Edge
 from pyspark import SparkConf, SparkContext, RDD
 
 
-def get_mst(V: List[Vertex], E: List[Edge]):
+def get_mst(V: List[Vertex], E: List[Edge], leader_size: int):
     # V is a set of vertices structured as an array of (x, y)
     # E is a set of edges structured as an array of ((x,y), (x,y))
 
-    data = []
-
-    sparkConf = SparkConf().setAppName('BoruvkaMST')
-    sparkContext = SparkContext(conf=sparkConf)
-    
-    sparkContext
-    
-    distData = sparkContext.parallelize(data)
-    distData.map()
-
-    return
+    while True:
+        best_neighbors: List[Tuple[Vertex, Vertex]] = find_best_neighbors_mpc(V, E)
+        dht_add_nearest_neighbors(best_neighbors)
+        V, E = contraction_mpc(V, E)
+        if len(V) <= leader_size:
+            return (V, E)
 
 # FindBestNeighbors-MPC implementation using PySpark
-def find_best_neighbors_mpc(V: List[Vertex], E: List[Edge]) -> Tuple[Vertex, Vertex]:
+def find_best_neighbors_mpc(V: List[Vertex], E: List[Edge]) -> List[Tuple[Vertex, Vertex]]:
     sparkConf = SparkConf().setAppName('BoruvkaMST_FindBestNeighbors')
     sparkContext = SparkContext(conf=sparkConf)
-    data = []
-    map_rdd = sparkContext.parallelize(data).map(lambda x: find_best_neighbors_map(x)) # key and values?
-    red_rdd = map_rdd.reduceByKey(lambda x: find_best_neighbors_red(x))
+    vertices_with_neighbors = get_vertex_with_neighbors(V, E)
+
+    map_rdd = sparkContext.parallelize(vertices_with_neighbors).map(lambda x: find_best_neighbors_map(x)) # key and values?
+    red_rdd = map_rdd.reduceByKey(lambda vertex_id, neighbor: find_best_neighbors_red(vertex_id, neighbor))
+    return red_rdd.collect()
+
+    # return red_rdd.collect()
+
+
+def get_vertex_with_neighbors(vertices: List[Vertex], edges: List[Edge]) -> List[(Vertex, List[Edge])]:
+    for i in range(len(vertices)):
+        current_vertex = vertices[i]
+        yield (current_vertex, get_distinct_neigbors(current_vertex, edges))
+
+
+def get_distinct_neigbors(vertex: Vertex, edges: List[Edge]) -> List[Edge]: 
+    return get_unique_edges(get_neighbors(vertex, edges))
     
 
-    return red_rdd.collect()
+def get_neighbors(vertex: Vertex, edges: List[Edge]):
+    for i in range(len(edges)):
+        current_edge = edges[i]
+        if current_edge.contains_vertex(vertex):
+            yield current_edge
+
 
 
 
@@ -53,6 +67,25 @@ def get_minimum_weight_vertex_of_neighbors(neighbors: List[Edge]):
 # FindBestNeighbors-MPC reduce
 def find_best_neighbors_red(vertex: Vertex, closest_neighbor: Edge):
     return (vertex, closest_neighbor)
+
+
+# Contraction-MPC 
+def contraction_mpc(vertices: List[Vertex], edges: List[Edge]) -> Tuple[List[Vertex], List[Edge]]:
+    sparkConf = SparkConf().setAppName('BoruvkaMST_FindBestNeighbors')
+    sparkContext = SparkContext(conf=sparkConf)
+    vertices_with_neighbors = get_vertex_with_neighbors(vertices, edges)
+    graph_tuples: List[Tuple[Vertex, List[Edge]]] = sparkContext.parallelize(vertices_with_neighbors).map(lambda x: contraction_map(x)).reduceByKey(lambda leader, neigbors: contraction_red(leader, neigbors)).collect()
+    
+    #combines tuples into graph
+    prime_vertices: List[Vertex] = []
+    prime_edges: List[List[Vertex]] = []
+    for i in range(len(graph_tuples)):
+        g_tuple = graph_tuples[i]
+        c_vertex = g_tuple[0]
+        c_edges = vertices[1]
+        prime_vertices.append(c_vertex)
+        prime_edges.append(c_edges)
+    return (prime_vertices, get_unique_edges(flat_map_edges(prime_edges)))
 
 
 # Contraction-MPC map
@@ -139,8 +172,16 @@ nearest_neighbor_dict = {}
 def dht_get_nearest_neighbor(vertex) -> Vertex:
     return nearest_neighbor_dict[vertex.id]
 
-def dht_add_nearest_neighbor(vertex: Vertex, nearest_neighbour: Vertex):
-    nearest_neighbor_dict[vertex.id] = nearest_neighbour
+def dht_add_nearest_neighbors(tuples: List[Tuple[Vertex, Vertex]]):
+    for i in range(len(tuples)):
+        vertex, leader = tuples[i]
+        dht_add_nearest_neighbor(vertex, leader)
+    
+def dht_add_nearest_neighbor(vertex: Vertex, neighbor: Vertex):
+    nearest_neighbor_dict[vertex.id] = neighbor
+
+def dht_reset_nearest_neighbor():
+    nearest_neighbor_dict = {}
 
 leader_dict = {}
 
@@ -149,3 +190,6 @@ def dht_get_leader_of_vertex(vertex: Vertex) -> Vertex:
     
 def dht_add_leader_of_vertex(key_vertex: Vertex, leader_vertex: Vertex):
     leader_dict[key_vertex.id] = leader_vertex
+
+def dht_reset_leader_of_vertex():
+    nearest_neighbor_dict = {}
